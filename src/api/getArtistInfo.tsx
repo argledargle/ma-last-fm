af6f5d7ae74470d7911378e44c3450ed62996143
argledargle <48493290+argledargle@users.noreply.github.com>
2021-09-23 01:27:59 +0000

    Deploying to react-branch from  @ 56629b64bc96162aefbbf6a766bcd61ccbcdcfcf 🚀

--- src/api/getArtistInfo.tsx
+++
@@ -1,7 +0,0 @@
-export const getArtistInfo = async (searchTerm: string, callback: Function) => {
-  await fetch(
-    `https://ws.audioscrobbler.com/2.0/?method=artist.getInfo&artist=${searchTerm}&api_key=6850c857aa62a615c85ecc093e32c107&format=json`
-  )
-    .then(response => response.json())
-    .then(data => callback(data))
-}
