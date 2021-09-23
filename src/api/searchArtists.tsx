ad59e1bbdd28deb1a0d46829ecf235641cc6210a
Alex <alexpeter@northwesternmutual.com>
2021-09-22 20:18:59 -0500

    fix: updates deploy + apikey

--- src/api/searchArtists.tsx
+++ src/api/searchArtists.tsx
@@ -1,6 +1,6 @@
 export const searchArtists = async (searchTerm: string, callback: Function) => {
   await fetch(
-    `http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${searchTerm}&api_key=${process.env.REACT_APP_APIKEY}&format=json`
+    `http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${searchTerm}&api_key=6850c857aa62a615c85ecc093e32c107&format=json`
   )
     .then(response => response.json())
     .then(data => callback(data))
